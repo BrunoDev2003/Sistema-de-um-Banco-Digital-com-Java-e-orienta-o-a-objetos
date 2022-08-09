97ce426b5dc5ed37682c03200f705751e92a5e35
Bruno Gusmão <brunogusmao43@gmail.com>
2022-08-09 17:40:32 -0300

    desenvolvendo saque,deposito e transferir

--- DIO-Banco/src/Conta.java
+++ DIO-Banco/src/Conta.java
@@ -3,26 +3,30 @@ public abstract class Conta implements IConta {
     protected int agencia; 
     protected int numero; 
     protected int saldo;
+    private static int SEQUENCIAL = 1;
     
-
+    public Conta() {
+        this.agencia = Conta.AGENCIA_PADRAO;
+        this.numero = SEQUENCIAL++;
+    }
 
 
     @Override
     public void depositar(double valor) {
-        // TODO Auto-generated method stub
+        saldo += valor;
         
     }
 
     @Override
     public void sacar(double valor) {
-        // TODO Auto-generated method stub
+        saldo -= valor;
         
     }
 
     @Override
-    public void transferir(Conta contaDestino) {
-        // TODO Auto-generated method stub
-        
+    public void transferir(double valor, Conta contaDestino) {
+        this.sacar(valor);
+        contaDestino.depositar(valor);
     }
 
     public int getAgencia() {
